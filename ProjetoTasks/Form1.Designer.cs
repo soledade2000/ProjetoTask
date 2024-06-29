2256cf91ef3e10f601f91271492a9464ecc6e25a
Gabriel Soledade <soledader2000@gmail.com>
2024-06-29 16:35:46 -0300

    ProjetoTask

---
+++ ProjetoTasks/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ProjetoTasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewTarefas = new System.Windows.Forms.DataGridView();
+            this.chkCompleta = new System.Windows.Forms.CheckBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTarefas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Gadugi", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(256, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tarefas";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // dataGridViewTarefas
+            // 
+            this.dataGridViewTarefas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTarefas.Location = new System.Drawing.Point(545, 137);
+            this.dataGridViewTarefas.Name = "dataGridViewTarefas";
+            this.dataGridViewTarefas.Size = new System.Drawing.Size(231, 117);
+            this.dataGridViewTarefas.TabIndex = 2;
+            this.dataGridViewTarefas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTarefas_CellClick);
+            this.dataGridViewTarefas.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTarefas_CellClick);
+            // 
+            // chkCompleta
+            // 
+            this.chkCompleta.AutoSize = true;
+            this.chkCompleta.Location = new System.Drawing.Point(368, 327);
+            this.chkCompleta.Name = "chkCompleta";
+            this.chkCompleta.Size = new System.Drawing.Size(70, 17);
+            this.chkCompleta.TabIndex = 3;
+            this.chkCompleta.Text = "Completa";
+            this.chkCompleta.UseVisualStyleBackColor = true;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnInserir.Location = new System.Drawing.Point(105, 350);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(68, 50);
+            this.btnInserir.TabIndex = 4;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = false;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(86, 119);
+            this.txtID.Multiline = true;
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(32, 33);
+            this.txtID.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(83, 170);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 19);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Título da Taefa:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(159, 19);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Descrição da tarefa:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(86, 192);
+            this.txtTitulo.Multiline = true;
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(158, 26);
+            this.txtTitulo.TabIndex = 8;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(86, 254);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(259, 90);
+            this.txtDescricao.TabIndex = 9;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.LightCoral;
+            this.btnExcluir.Location = new System.Drawing.Point(256, 350);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(68, 50);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Gadugi", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(627, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 19);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Tarefa";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(631, 260);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(68, 50);
+            this.btnAtualizar.TabIndex = 12;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.chkCompleta);
+            this.Controls.Add(this.dataGridViewTarefas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTarefas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewTarefas;
+        private System.Windows.Forms.CheckBox chkCompleta;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAtualizar;
+    }
+}
+
---
+++ ProjetoTasks/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoTasks
+{
+    public partial class Form1 : Form
+    {
+        // String de conexão com o banco de dados 
+        private string connectionString = "Server=DESKTOP-HVL8FQ6\\MSSQLSERVER02;Database=TaskManager;Integrated Security=True;";
+
+        // ID da tarefa selecionada para atualização ou exclusão
+        private int selectedTaskId = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Click += Form1_Click; // Adiciona o evento Click ao formulário para limpar TextBoxes
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // Carrega as tarefas ao iniciar o formulário
+            await AtualizarGridTarefasAsync();
+        }
+
+        // Método para carregar as tarefas na DataGridView
+        private async Task AtualizarGridTarefasAsync()
+        {
+            dataGridViewTarefas.DataSource = null; // Limpa os dados existentes na DataGridView
+            dataGridViewTarefas.Rows.Clear();
+
+            string query = "SELECT * FROM Tasks"; // Consulta SQL para selecionar todas as tarefas
+
+            try
+            {
+                DataTable dataTable = await SelectAsync(query); // Executa a consulta 
+                dataGridViewTarefas.DataSource = dataTable; // Define os dados da DataGridView como o resultado da consulta
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar tarefas: " + ex.Message); // Exibe mensagem de erro se ocorrer uma exceção
+            }
+        }
+
+        // Método para executar consultas SELECT no banco de dados 
+        private async Task<DataTable> SelectAsync(string query)
+        {
+            using (var connection = await GetOpenConnectionAsync()) // Abre a conexão com o banco de dados 
+            using (var command = new SqlCommand(query, connection))
+            {
+                var dataTable = new DataTable();
+                using (var reader = await command.ExecuteReaderAsync()) // Executa o comando de consulta 
+                {
+                    dataTable.Load(reader); // Carrega os resultados da consulta para um DataTable
+                }
+                return dataTable; // Retorna o DataTable com os resultados da consulta
+            }
+        }
+
+        // Método para executar comandos INSERT, UPDATE ou DELETE no banco de dados 
+        private async Task<int> ExecuteNonQueryAsync(string query, SqlParameter[] parameters)
+        {
+            using (var connection = await GetOpenConnectionAsync()) // Abre a conexão com o banco de dados 
+            using (var command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters); // Adiciona os parâmetros ao comando
+                }
+                return await command.ExecuteNonQueryAsync(); // Executa o comando  e retorna o número de linhas afetadas
+            }
+        }
+
+        // Método para obter e abrir uma conexão com o banco de dados 
+        private async Task<SqlConnection> GetOpenConnectionAsync()
+        {
+            var connection = new SqlConnection(connectionString); // Cria uma nova instância de conexão com o banco de dados
+            await connection.OpenAsync(); // Abre a conexão 
+            return connection; // Retorna a conexão aberta
+        }
+
+        // Método para limpar as TextBoxes e redefinir o ID da tarefa selecionada
+        private void LimparTextBoxes()
+        {
+            txtID.Text = string.Empty; // Limpa o campo de ID da tarefa
+            txtTitulo.Text = string.Empty; // Limpa o campo de título da tarefa
+            txtDescricao.Text = string.Empty; // Limpa o campo de descrição da tarefa
+            selectedTaskId = -1; // Reseta o ID da tarefa selecionada
+            chkCompleta.Checked = false; // Desmarca a CheckBox de tarefa completa
+        }
+
+        // Evento Click do formulário para limpar as TextBoxes quando o usuário clicar fora da DataGridView
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            // Verifica se o clique foi fora da DataGridView
+            Point pt = dataGridViewTarefas.PointToClient(Cursor.Position); // Converte as coordenadas do cursor para coordenadas relativas à DataGridView
+            DataGridView.HitTestInfo hit = dataGridViewTarefas.HitTest(pt.X, pt.Y); // Obtém informações sobre o elemento da DataGridView clicado
+
+            if (hit.Type == DataGridViewHitTestType.None)
+            {
+                LimparTextBoxes(); // Limpa as TextBoxes se o clique foi fora da DataGridView
+            }
+        }
+
+        // Evento CellClick da DataGridView para selecionar uma tarefa e exibir seus detalhes nas TextBoxes
+        private void dataGridViewTarefas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewTarefas.Rows[e.RowIndex]; // Obtém a linha da DataGridView clicada
+                selectedTaskId = Convert.ToInt32(row.Cells["TaskId"].Value); // Obtém o ID da tarefa selecionada
+
+                // Preenche as TextBoxes com os dados da tarefa selecionada, tratando DBNull se necessário
+                txtID.Text = selectedTaskId.ToString();
+                txtTitulo.Text = row.Cells["Title"].Value == DBNull.Value ? string.Empty : row.Cells["Title"].Value.ToString();
+                txtDescricao.Text = row.Cells["Description"].Value == DBNull.Value ? string.Empty : row.Cells["Description"].Value.ToString();
+                chkCompleta.Checked = row.Cells["IsCompleted"].Value != DBNull.Value && Convert.ToBoolean(row.Cells["IsCompleted"].Value);
+            }
+            else
+            {
+                LimparTextBoxes(); // Limpa as TextBoxes se clicar fora da tabela
+            }
+        }
+
+        // Evento Click do botão "Inserir" para adicionar uma nova tarefa ao banco de dados
+        private async void btnInserir_Click(object sender, EventArgs e)
+        {
+            string titulo = txtTitulo.Text.Trim(); // Obtém o título da nova tarefa
+            string descricao = txtDescricao.Text.Trim(); // Obtém a descrição da nova tarefa
+
+            if (!string.IsNullOrEmpty(titulo) && !string.IsNullOrEmpty(descricao))
+            {
+                string query = "INSERT INTO Tasks (Title, Description, IsCompleted, CreatedDate) VALUES (@Title, @Description, @IsCompleted, @CreatedDate)";
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter("@Title", titulo),
+                    new SqlParameter("@Description", descricao),
+                    new SqlParameter("@IsCompleted", false),
+                    new SqlParameter("@CreatedDate", DateTime.Now)
+                };
+
+                try
+                {
+                    int rowsAffected = await ExecuteNonQueryAsync(query, parameters); // Executa o comando de inserção de forma assíncrona
+                    MessageBox.Show($"Tarefa inserida com sucesso. Linhas afetadas: {rowsAffected}");
+                    await AtualizarGridTarefasAsync(); // Atualiza a DataGridView com as novas tarefas após a inserção
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao inserir tarefa: " + ex.Message); // Exibe mensagem de erro se ocorrer uma exceção
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira o título e a descrição da tarefa."); // Exibe mensagem se o título ou descrição estiverem vazios
+            }
+        }
+
+        // Evento Click do botão "Atualizar" para atualizar uma tarefa existente no banco de dados
+        private async void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            if (selectedTaskId < 0)
+            {
+                MessageBox.Show("Selecione uma tarefa para atualizar."); // Exibe mensagem se nenhuma tarefa estiver selecionada
+                return;
+            }
+
+            string titulo = txtTitulo.Text.Trim(); // Obtém o novo título da tarefa
+            string descricao = txtDescricao.Text.Trim(); // Obtém a nova descrição da tarefa
+            bool isCompleted = chkCompleta.Checked; // Obtém o novo status de conclusão da tarefa
+
+            if (!string.IsNullOrEmpty(titulo) && !string.IsNullOrEmpty(descricao))
+            {
+                string query = "UPDATE Tasks SET Title = @Title, Description = @Description, IsCompleted = @IsCompleted WHERE TaskId = @TaskId";
+                SqlParameter[] parameters = new SqlParameter[]
+                {
+                    new SqlParameter("@Title", titulo),
+                    new SqlParameter("@Description", descricao),
+                    new SqlParameter("@IsCompleted", isCompleted),
+                    new SqlParameter("@TaskId", selectedTaskId)
+                };
+
+                try
+                {
+                    int rowsAffected = await ExecuteNonQueryAsync(query, parameters); // Executa o comando de atualização 
+                    MessageBox.Show($"Tarefa atualizada com sucesso. Linhas afetadas: {rowsAffected}");
+                    await AtualizarGridTarefasAsync(); // Atualiza a DataGridView após a atualização da tarefa
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao atualizar tarefa: " + ex.Message); // Exibe mensagem de erro se ocorrer uma exceção
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira o título e a descrição da tarefa."); // Exibe mensagem se o título ou descrição estiverem vazios
+            }
+        }
+
+        // Evento Click do botão "Excluir" para excluir uma tarefa do banco de dados
+        private async void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (selectedTaskId < 0)
+            {
+                MessageBox.Show("Selecione uma tarefa para excluir."); // Exibe mensagem se nenhuma tarefa estiver selecionada
+                return;
+            }
+
+            string query = "DELETE FROM Tasks WHERE TaskId = @TaskId";
+            SqlParameter parameter = new SqlParameter("@TaskId", selectedTaskId);
+
+            try
+            {
+                int rowsAffected = await ExecuteNonQueryAsync(query, new SqlParameter[] { parameter }); // Executa o comando de exclusão 
+                MessageBox.Show($"Tarefa excluída com sucesso. Linhas afetadas: {rowsAffected}");
+                await AtualizarGridTarefasAsync(); // Atualiza a DataGridView após a exclusão da tarefa
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao excluir tarefa: " + ex.Message); // Exibe mensagem de erro se ocorrer uma exceção
+            }
+        }
+    }
+}
